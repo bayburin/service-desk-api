96b63873092f6a229d09a4441963c061d40b795c
Байбурин Равиль <bayburin@iss-reshetnev.ru>
2019-04-30 19:23:18 +0700

    Небольшие изменения в конфигурации ThinkingSphinx.

--- app/indices/categories_index.rb
+++ app/indices/categories_index.rb
@@ -2,5 +2,5 @@ ThinkingSphinx::Index.define :category, with: :active_record, delta: true do
   indexes name, sortable: true
   indexes short_description
 
-  has popularity
+  has popularity, type: :integer
 end
--- app/indices/tickets_index.rb
+++ app/indices/tickets_index.rb
@@ -2,7 +2,8 @@ ThinkingSphinx::Index.define :ticket, with: :active_record, delta: true do
   indexes name, sortable: true
   indexes tags.name, as: :tag_name
 
-  has popularity
+  has popularity, type: :integer
+  has ticket_type, type: :integer
 
   where 'ticket_type != 3'
 end
