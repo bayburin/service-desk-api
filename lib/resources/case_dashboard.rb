1008c28499f14b8c92dd2776d03e3ad3f76d937a
Байбурин Равиль <bayburin@iss-reshetnev.ru>
2019-05-05 09:42:39 +0700

    Изменен класс CaseDashboard и соответствующий сериалайзер.

--- lib/resources/case_dashboard.rb
+++ lib/resources/case_dashboard.rb
@@ -2,11 +2,10 @@ class CaseDashboard
   include ActiveModel::Model
   include ActiveModel::Serialization
 
-  attr_accessor :cases, :statuses, :case_count
+  attr_accessor :cases, :statuses
 
   def initialize(args = {})
     @cases = args['cases']
     @statuses = args['statuses']
-    @case_count = args['case_count']
   end
 end
