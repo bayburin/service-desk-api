72173e153b04fd2bb13059f1dbef7b339fdda027
Байбурин Равиль <bayburin@iss-reshetnev.ru>
2019-05-21 13:47:51 +0700

    Изменен ticket_serializer: отключены теги, ответы на вопросы и связанная услуга теперь показываются всегда.

--- app/serializers/ticket_serializer.rb
+++ app/serializers/ticket_serializer.rb
@@ -1,10 +1,9 @@
 class TicketSerializer < ActiveModel::Serializer
   attributes :id, :service_id, :name, :ticket_type, :is_hidden, :sla, :popularity
 
-  has_many :answers, if: :include_associations?
-  has_many :tags, if: :include_associations?
+  has_many :answers
 
-  belongs_to :service, if: :include_associations?
+  belongs_to :service
 
   def include_associations?
     !object.without_associations
