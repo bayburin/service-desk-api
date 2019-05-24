706d73181dbb5317f3ec30a00a1567c20a1ae255
Байбурин Равиль <bayburin@iss-reshetnev.ru>
2019-05-24 13:24:16 +0700

    Изменен путь хранилища для загрузки файлов.

--- app/uploaders/answer_attachment_uploader.rb
+++ app/uploaders/answer_attachment_uploader.rb
@@ -11,7 +11,7 @@ class AnswerAttachmentUploader < CarrierWave::Uploader::Base
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     # "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-    Rails.root + "tmp/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    Rails.root + "storage/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
