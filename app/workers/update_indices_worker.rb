c628e64bc343f37fa2084fdf0aff0734f0547de4
Байбурин Равиль <bayburin@iss-reshetnev.ru>
2019-05-29 13:55:53 +0700

    Изменения в приложении для работы в production режиме. Изменены настройки деплоя.

--- app/workers/update_indices_worker.rb
+++ app/workers/update_indices_worker.rb
@@ -2,6 +2,6 @@ class UpdateIndicesWorker
   include Sidekiq::Worker
 
   def perform
-    system('rails ts:merge')
+    system('rails ts:index')
   end
 end
---
+++ config/deploy.rb
@@ -0,0 +1,23 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.11.0"
+
+server 'service-desk', user: 'deployer', roles: %w[web app db]
+
+set :repo_url,        'git@gitlab.iss-reshetnev.ru:714/7141/service-desk-api.git'
+set :ssh_options,     forward_agent: false, user: 'deployer'
+set :use_sudo,        false
+set :rbenv_ruby,      '2.5.5'
+set :rbenv_map_bins,  fetch(:rbenv_map_bins).to_a.concat(%w[rake gem bundle ruby rails])
+set :linked_files,    %w[config/database.yml .env]
+set :linked_dirs,     %w[log tmp/pids tmp/cache vendor/bundle]
+
+namespace :deploy do
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      invoke 'unicorn:restart'
+    end
+  end
+
+  after :publishing, :restart
+end
