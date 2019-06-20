6f582d57cebc222fbf2962dd270105b604e6c21d
Байбурин Равиль <bayburin@iss-reshetnev.ru>
2019-06-20 16:37:12 +0700

    Добавлен вывод в логи IP адреса, с которого пришел запрос

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::API
+  before_action :log_ip
+
   def welcome
     render :nothing
+  end
+
+  protected
+
+  def log_ip
+    logger.info "Request from IP: #{request.env['HTTP_X_FORWARDED_FOR']}"
   end
 end
