9c2ef95089864d936180c5c686370bc7405a3da3
Байбурин Равиль <bayburin@iss-reshetnev.ru>
2020-01-30 11:41:08 +0700

    Исправлен баг, при котором в уведомительных письмах об изменении вопросов не указывалось ФИО ответственного, внесшего изменения.

--- app/workers/api/v1/notify_content_managers_worker.rb
+++ app/workers/api/v1/notify_content_managers_worker.rb
@@ -3,26 +3,26 @@ module Api
     class NotifyContentManagersWorker
       include Sidekiq::Worker
 
-      def perform(ticket_id, current_user_id, type, origin)
+      def perform(ticket_id, current_user_tn, type, origin)
         manager_ids = UsersQuery.new.managers.pluck(:id)
 
         case type
         when 'create'
-          notify_on_create(manager_ids, ticket_id, current_user_id, origin)
+          notify_on_create(manager_ids, ticket_id, current_user_tn, origin)
         when 'update'
-          notify_on_update(manager_ids, ticket_id, current_user_id, origin)
+          notify_on_update(manager_ids, ticket_id, current_user_tn, origin)
         end
       end
 
-      def notify_on_create(manager_ids, ticket_id, current_user_id, origin)
+      def notify_on_create(manager_ids, ticket_id, current_user_tn, origin)
         manager_ids.each do |manager_id|
-          Api::V1::NotifyContentManagerOnCreate.perform_async(manager_id, ticket_id, current_user_id, origin)
+          Api::V1::NotifyContentManagerOnCreate.perform_async(manager_id, ticket_id, current_user_tn, origin)
         end
       end
 
-      def notify_on_update(manager_ids, ticket_id, current_user_id, origin)
+      def notify_on_update(manager_ids, ticket_id, current_user_tn, origin)
         manager_ids.each do |manager_id|
-          Api::V1::NotifyContentManagerOnUpdate.perform_async(manager_id, ticket_id, current_user_id, origin)
+          Api::V1::NotifyContentManagerOnUpdate.perform_async(manager_id, ticket_id, current_user_tn, origin)
         end
       end
     end
--- spec/workers/api/v1/notify_content_manager_on_create_spec.rb
+++ spec/workers/api/v1/notify_content_manager_on_create_spec.rb
@@ -9,7 +9,8 @@ module Api
       let(:sender) { double(ReportSender, send_report: true) }
       before do
         allow(User).to receive(:find).with(manager.id).and_return(manager)
-        allow(User).to receive(:find).with(operator.id).and_return(operator)
+        # allow(User).to receive(:find).with(operator.id).and_return(operator)
+        allow(User).to receive(:authenticate).with(tn: operator.tn).and_return(operator)
         allow(manager).to receive(:load_details).and_return(manager)
         allow(operator).to receive(:load_details).and_return(operator)
         allow(ReportSender).to receive(:new).and_return(sender)
@@ -19,25 +20,25 @@ module Api
       it 'find user with specified id' do
         expect(User).to receive(:find).with(manager.id).and_return(manager)
 
-        subject.perform(manager.id, ticket.id, operator.id, '')
+        subject.perform(manager.id, ticket.id, operator.tn, '')
       end
 
       it 'calls #load_details method for finded user' do
         expect(manager).to receive(:load_details)
 
-        subject.perform(manager.id, ticket.id, operator.id, '')
+        subject.perform(manager.id, ticket.id, operator.tn, '')
       end
 
       it 'creates instance of ReportSender' do
         expect(ReportSender).to receive(:new).with(manager, ticket, operator, '').and_return(sender)
 
-        subject.perform(manager.id, ticket.id, operator.id, '')
+        subject.perform(manager.id, ticket.id, operator.tn, '')
       end
 
       it 'calls #send_report method for ReportSender instance' do
         expect(sender).to receive(:send_report).with(an_instance_of(Tickets::TicketUpdatedEmailSender))
 
-        subject.perform(manager.id, ticket.id, operator.id, '')
+        subject.perform(manager.id, ticket.id, operator.tn, '')
       end
     end
   end
