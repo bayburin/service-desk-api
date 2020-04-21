6cc92ae8ebf75353b77005dda6e3075462fbc76b
Байбурин Равиль <baiburinrf@gmail.com>
2020-04-22 01:22:25 +0700

    Изменен метод #attributes_for_deep_search для админских ролей

--- app/policies/question_ticket_policy.rb
+++ app/policies/question_ticket_policy.rb
@@ -2,9 +2,9 @@ class QuestionTicketPolicy < TicketPolicy
   def attributes_for_deep_search
     if user.one_of_roles?(:content_manager, :operator, :service_responsible)
       PolicyAttributes.new(
-        serializer: Api::V1::Tickets::TicketResponsibleUserSerializer,
-        sql_include: [:responsible_users, service: :responsible_users, answers: :attachments],
-        serialize: ['responsible_users', 'service.responsible_users', 'answers.attachments']
+        serializer: Api::V1::QuestionTickets::QuestionTicketResponsibleUserSerializer,
+        sql_include: [:responsible_users, ticket: { service: :responsible_users }, answers: :attachments],
+        serialize: ['responsible_users', 'ticket.service.responsible_users', 'answers.attachments']
       )
     else
       PolicyAttributes.new(
--- spec/policies/question_ticket_policy_spec.rb
+++ spec/policies/question_ticket_policy_spec.rb
@@ -11,53 +11,53 @@ RSpec.describe QuestionTicketPolicy do
   describe '#attributes_for_deep_search' do
     let(:question) { service.question_tickets.first }
 
-    # context 'for user with :service_responsible role' do
-    #   subject(:policy) { TicketPolicy.new(responsible, ticket).attributes_for_deep_search }
+    context 'for user with :service_responsible role' do
+      subject(:policy) { QuestionTicketPolicy.new(responsible, question).attributes_for_deep_search }
 
-    #   it 'sets :serializer attribute' do
-    #     expect(policy.serializer).to eq Api::V1::Tickets::TicketResponsibleUserSerializer
-    #   end
+      it 'sets :serializer attribute' do
+        expect(policy.serializer).to eq Api::V1::QuestionTickets::QuestionTicketResponsibleUserSerializer
+      end
 
-    #   it 'sets :sql_include attribute' do
-    #     expect(policy.sql_include).to eq [:responsible_users, service: :responsible_users, answers: :attachments]
-    #   end
+      it 'sets :sql_include attribute' do
+        expect(policy.sql_include).to eq [:responsible_users, ticket: { service: :responsible_users }, answers: :attachments]
+      end
 
-    #   it 'sets :serialize attribute' do
-    #     expect(policy.serialize).to eq ['responsible_users', 'service.responsible_users', 'answers.attachments']
-    #   end
-    # end
+      it 'sets :serialize attribute' do
+        expect(policy.serialize).to eq ['responsible_users', 'ticket.service.responsible_users', 'answers.attachments']
+      end
+    end
 
-    # context 'for user with :operator role' do
-    #   subject(:policy) { TicketPolicy.new(operator, ticket).attributes_for_deep_search }
+    context 'for user with :operator role' do
+      subject(:policy) { QuestionTicketPolicy.new(operator, question).attributes_for_deep_search }
 
-    #   it 'sets :serializer attribute' do
-    #     expect(policy.serializer).to eq Api::V1::Tickets::TicketResponsibleUserSerializer
-    #   end
+      it 'sets :serializer attribute' do
+        expect(policy.serializer).to eq Api::V1::QuestionTickets::QuestionTicketResponsibleUserSerializer
+      end
 
-    #   it 'sets :sql_include attribute' do
-    #     expect(policy.sql_include).to eq [:responsible_users, service: :responsible_users, answers: :attachments]
-    #   end
+      it 'sets :sql_include attribute' do
+        expect(policy.sql_include).to eq [:responsible_users, ticket: { service: :responsible_users }, answers: :attachments]
+      end
 
-    #   it 'sets :serialize attribute' do
-    #     expect(policy.serialize).to eq ['responsible_users', 'service.responsible_users', 'answers.attachments']
-    #   end
-    # end
+      it 'sets :serialize attribute' do
+        expect(policy.serialize).to eq ['responsible_users', 'ticket.service.responsible_users', 'answers.attachments']
+      end
+    end
 
-    # context 'for user with :content_manager role' do
-    #   subject(:policy) { TicketPolicy.new(content_manager, ticket).attributes_for_deep_search }
+    context 'for user with :content_manager role' do
+      subject(:policy) { QuestionTicketPolicy.new(content_manager, question).attributes_for_deep_search }
 
-    #   it 'sets :serializer attribute' do
-    #     expect(policy.serializer).to eq Api::V1::Tickets::TicketResponsibleUserSerializer
-    #   end
+      it 'sets :serializer attribute' do
+        expect(policy.serializer).to eq Api::V1::QuestionTickets::QuestionTicketResponsibleUserSerializer
+      end
 
-    #   it 'sets :sql_include attribute' do
-    #     expect(policy.sql_include).to eq [:responsible_users, service: :responsible_users, answers: :attachments]
-    #   end
+      it 'sets :sql_include attribute' do
+        expect(policy.sql_include).to eq [:responsible_users, ticket: { service: :responsible_users }, answers: :attachments]
+      end
 
-    #   it 'sets :serialize attribute' do
-    #     expect(policy.serialize).to eq ['responsible_users', 'service.responsible_users', 'answers.attachments']
-    #   end
-    # end
+      it 'sets :serialize attribute' do
+        expect(policy.serialize).to eq ['responsible_users', 'ticket.service.responsible_users', 'answers.attachments']
+      end
+    end
 
     context 'for user with any another role' do
       subject(:policy) { QuestionTicketPolicy.new(guest, question).attributes_for_deep_search }
