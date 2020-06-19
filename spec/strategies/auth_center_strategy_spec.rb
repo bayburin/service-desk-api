696e31c58424456894988804ea83d5db278fc14c
Байбурин Равиль <bayburin@iss-reshetnev.ru>
2020-06-19 16:06:33 +0700

    Изменена стратегия авторизации. Теперь токены хранятся в redis

--- spec/strategies/auth_center_strategy_spec.rb
+++ spec/strategies/auth_center_strategy_spec.rb
@@ -5,32 +5,16 @@ RSpec.describe AuthCenterStrategy, type: :request do
     let!(:user) { create(:user) }
     let(:access_token) { 'my_access_token' }
     let(:headers) { { Authorization: "Bearer #{access_token}" }.as_json }
-    let(:auth_center_response) do
-      {
-        token_type: 'Bearer',
-        expires_in: 123_456_789,
-        access_token: access_token,
-        refresh_token: 'my_refresh_token'
-      }
-    end
-    before do
-      stub_request(:get, 'https://auth-center.iss-reshetnev.ru/api/module/main/login_info')
-        .to_return(status: 200, body: user.to_json, headers: {})
-    end
+    let(:access_token) { 'my_token' }
+    before { Api::V1::AuthCenter::AccessToken.set(access_token, user.as_json) }
 
-    it 'runs #access_token method for Api::V1::AuthCenterApi' do
-      expect(Api::V1::AuthCenterApi).to receive(:user_info).with(access_token).and_call_original
-
-      get '/api/v1/welcome.json', headers: headers
-    end
-
-    it 'runs #authenticate method for User' do
+    it 'call #authenticate method for User' do
       expect(User).to receive(:authenticate).with(user.as_json).and_call_original
 
       get '/api/v1/welcome.json', headers: headers
     end
 
-    it 'runs #success! method with User instance for AuthCenterStrategy instance' do
+    it 'call #success! method with User instance for AuthCenterStrategy instance' do
       expect_any_instance_of(AuthCenterStrategy).to receive(:success!).with(user)
 
       get '/api/v1/welcome.json', headers: headers
@@ -47,12 +31,9 @@ RSpec.describe AuthCenterStrategy, type: :request do
     end
 
     context 'when authorization server respond with error' do
-      before do
-        stub_request(:get, 'https://auth-center.iss-reshetnev.ru/api/module/main/login_info')
-          .to_return(status: 401, body: { error: 'Not authorized' }.to_json, headers: {})
-      end
+      before { allow(Api::V1::AuthCenter::AccessToken).to receive(:get).and_return(nil) }
 
-      it 'returns with error message' do
+      it 'respond with access denied message' do
         get '/api/v1/welcome.json', headers: headers
 
         expect(response.body).to be_json_eql({ error: 'Невалидный токен' }.to_json)
